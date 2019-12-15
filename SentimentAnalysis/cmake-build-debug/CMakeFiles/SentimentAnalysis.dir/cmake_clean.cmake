04e6b380c382b73baf5332d55510ac9dfe523002
Haseeb <zenyc@live.com>
2019-12-15 18:01:04 +0500

    Files added through version control with CLion

---
+++ SentimentAnalysis/cmake-build-debug/CMakeFiles/SentimentAnalysis.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SentimentAnalysis.dir/main.cpp.o"
+  "SentimentAnalysis"
+  "SentimentAnalysis.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SentimentAnalysis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ SentimentAnalysis/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
